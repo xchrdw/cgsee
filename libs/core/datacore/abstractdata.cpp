32f198e64c71c6d8b9ac9cfa708aca1796200bb8
Pavel <pavel.belskiy@student.hpi.uni-potsdam.de>
2013-05-04 01:39:57 +0200

    Fleshing out datacore base classes

--- libs/core/datacore/abstractdata.cpp
+++ libs/core/datacore/abstractdata.cpp
@@ -2,9 +2,23 @@
 
 QString GlobalDataRegistry::registerNewData(QString proposedName, DataItem* data)
 {
-    m_dataMap.insert(proposedName, data);
+    static const QString extraFormat("%1%2");
+    QString realName = proposedName;
+    if (m_dataMap.contains(proposedName))
+    {
+        unsigned int i = 0;
+        do {
+            realName = extraFormat.arg(proposedName).arg(++i);
+        } while(m_dataMap.contains(realName));
+    }
+    m_dataMap.insert(realName, data);
     this->connect(data, SIGNAL(destroyed(QObject*)), SLOT(dataItemRemoved(QObject*)));
-    return proposedName;
+    return realName;
+}
+
+DataItem * GlobalDataRegistry::getDataItemByName(QString name)
+{
+    return m_dataMap[name];
 }
 
 void GlobalDataRegistry::dataItemRemoved(QObject* removedItem)
@@ -18,3 +32,20 @@ QString DataItem::name() const
 {
     return m_name;
 }
+
+DataItem::StandardPointer DataItem::clone(QString *newName, GlobalDataRegistry & registry)
+{
+    DataItem::StandardPointer newItem = new DataItem(*this);
+    QString chosenName = (nullptr == newName) ? name() : *newName;
+    chosenName = registry.registerNewData(chosenName, newItem);
+    if (newName)
+        *newName = chosenName;
+    return newItem;
+}
+
+DataItem::StandardPointer DataItem::clone(QString *newName)
+{
+    return clone(newName, GlobalDataRegistry::getGlobalInstance());
+}
+
+GlobalDataRegistry GlobalDataRegistry::s_instance;
--- libs/core/datacore/abstractdata.h
+++ libs/core/datacore/abstractdata.h
@@ -20,27 +20,56 @@ public:
     // returns the name under which the object has been saved
     QString registerNewData(QString proposedName, DataItem* data);
     DataItem* getDataItemByName(QString itemName);
+    static GlobalDataRegistry& getGlobalInstance()
+    {
+        return s_instance;
+    }
 
 public slots:
     // Notifies about destruction of a DataItem.
     void dataItemRemoved(QObject* dataItem);
 
 protected:
-    QMap<QString, DataItem*> m_dataMap;
+    QHash<QString, DataItem*> m_dataMap;
+
+private:
+    static GlobalDataRegistry s_instance;
 };
 
 class CGSEE_API DataItem: public QObject
 {
     Q_OBJECT
 public:
-    DataItem();
-    virtual ~DataItem();
+    virtual ~DataItem(){}
+
+    typedef DataItem * StandardPointer;
+
+    template<class DataItemSubclass>
+    static StandardPointer createDataItemWithName(QString &name, GlobalDataRegistry &registry)
+    {
+        DataItem result;
+        result.m_name = registry.registerNewData(name, &result);
+    }
+
+    // attempt to clone the item.
+    // it will be given a new name and saved in the registry provided.
+    virtual StandardPointer clone(QString *newName, GlobalDataRegistry & registry);
+    virtual StandardPointer clone(QString *newName);
 
     QString name() const;
     Q_PROPERTY(QString name READ name)
 protected:
     QString m_name;
+    
+    // Objects are created with static function createDataItemWithName which
+    // registers them automatically in the registry provided
+    DataItem(){}
 
+    // This is already done in QObject, but in case if anyone forgets:
+    // QObjects cannot be copied with a constructor
+    DataItem(const DataItem&){}
 };
+
+typedef DataItem::StandardPointer DataItemP;
 
 #endif //DATAINTERFACE_H
