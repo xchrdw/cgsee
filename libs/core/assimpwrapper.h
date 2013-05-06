c45c5675d12fcd9c6ef88b2adea6a69c57841550
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-05-07 01:07:57 +0200

    Revert "Revert "Add Assimp dependency and corresponding cmake lines; add assimpwrapper class in core/""
    This reverts commit adf2205cc6d4e3b01d1ba8bd4f968b98b65b79fb.

---
+++ libs/core/assimpwrapper.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h> 
+#include <assimp/postprocess.h>
+
+class CGSEE_API AssimpWrapper
+{
+private:
+  Assimp::Importer importer;  
+};
