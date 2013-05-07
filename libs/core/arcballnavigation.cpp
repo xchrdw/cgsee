889daf308eef400f14e5018bef17219cce2ef3cc
Christian Dullweber <xchrdw@gmail.com>
2013-05-08 00:01:02 +0200

    improve arcball code, add zoom #4

--- libs/core/arcballnavigation.cpp
+++ libs/core/arcballnavigation.cpp
@@ -16,27 +16,37 @@ void ArcballNavigation::reset()
 {
     m_center = glm::vec3(0, 0, 0);
     m_direction = glm::vec3(0, 0, -2);
-    m_last = m_cur = glm::vec2(0, 0);
+    m_mouse_last = m_mouse_cur = glm::vec2(0, 0);
     m_arcball_on = false;
     m_viewMatrix = glm::lookAt(m_center + m_direction, m_center, glm::vec3( 0.f, 1.f, 0.f));
+    m_viewMatrixInverted = glm::inverse(m_viewMatrix);
 }
 
 
 const glm::mat4 ArcballNavigation::viewMatrix() {
-    if (m_cur != m_last) {
-        //qDebug("update");
-        glm::vec3 va = get_arcball_vector(m_last);
-        glm::vec3 vb = get_arcball_vector(m_cur);
-        float angle = acos(std::min(1.0f, glm::dot(va, vb)));
-        glm::vec3 axis_in_camera_coord = glm::cross(va, vb);
+    if (m_mouse_cur != m_mouse_last) {
+        if(m_arcball_on)
+            updateArcball();
+        if(m_zoom_on)
+            updateZoom();
 
-        glm::mat3 camera2world = glm::inverse(glm::mat3(m_viewMatrix));
-        glm::vec3 axis_in_world_coord = camera2world * axis_in_camera_coord;
-        m_viewMatrix = m_viewMatrix * glm::rotate(glm::mat4(), angle*50, axis_in_world_coord);
-
-        m_last = m_cur;
+        m_viewMatrixInverted = glm::inverse(m_viewMatrix); // is needed in next call anyway
+        m_mouse_last = m_mouse_cur;
     }
     return m_viewMatrix;
+}
+
+void ArcballNavigation::updateArcball()
+{
+    glm::vec3 va = getArcballVector(m_mouse_last);
+    glm::vec3 vb = getArcballVector(m_mouse_cur);
+
+    float angle = acos(std::min(1.0f, glm::dot(va, vb)));
+    glm::vec3 axis_in_camera_coord = glm::cross(va, vb);
+
+    glm::mat3 camera2world = glm::mat3(m_viewMatrixInverted);
+    glm::vec3 axis_in_world_coord = camera2world * axis_in_camera_coord;
+    m_viewMatrix = m_viewMatrix * glm::rotate(glm::mat4(), glm::degrees(angle), axis_in_world_coord);
 }
 
 /**
@@ -44,47 +54,61 @@ const glm::mat4 ArcballNavigation::viewMatrix() {
  * point P on the virtual ball surface, such that P is aligned on
  * screen's (X,Y) coordinates.  If (X,Y) is too far away from the
  * sphere, return the nearest point on the virtual ball surface.
+ * source: http://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Arcball
  */
-glm::vec3 ArcballNavigation::get_arcball_vector(glm::vec2 v) {
+glm::vec3 ArcballNavigation::getArcballVector(glm::vec2 v) {
+  // normalize mouse coordinates to [-1:1]
   glm::vec3 P = glm::vec3(1.0 * v.x/m_width * 2 - 1.0,
                           1.0 * v.y/m_height * 2 - 1.0,
                           0);
   P.y = -P.y;
-  float OP_squared = P.x * P.x + P.y * P.y;
-  if (OP_squared <= 1*1)
-      P.z = sqrt(1*1 - OP_squared);
+  float P_squared = P.x * P.x + P.y * P.y;
+  if (P_squared <= 1*1) // P is inside ball
+      // calculate height in ball, small P_squared means P is near center -> high z value
+      P.z = sqrt(1*1 - P_squared); 
   else
-      P = glm::normalize(P);  // nearest point
+      P = glm::normalize(P);  // nearest point to the ball
   return P;
 }
 
+void ArcballNavigation::updateZoom()
+{
+    float delta = (m_mouse_cur - m_mouse_last).y / m_height; // normalized height difference
+    glm::vec3 zoom_in_camera = glm::vec3(0, 0, -delta) * glm::mat3(m_viewMatrix) ; // TODO relative to z-buffer
+    
+    glm::mat4 translate = glm::translate(glm::mat4(), zoom_in_camera);
+    m_viewMatrix = m_viewMatrix * translate;
+}
+
+
 void ArcballNavigation::mouseMoveEvent(QMouseEvent * event)
 {
-    if (m_arcball_on) {
-        m_cur = glm::vec2(event->pos().x(), event->pos().y());
+    if (m_arcball_on || m_zoom_on) {
+        m_mouse_cur = glm::vec2(event->pos().x(), event->pos().y());
     }
 }
 
 void ArcballNavigation::mousePressEvent(QMouseEvent * event)
 {
-    qDebug("press %i %i", event->pos().x(), event->pos().y());
-
-    if (event->button() == Qt::LeftButton) {
-        m_arcball_on = true;
-        m_last = m_cur = glm::vec2(event->pos().x(), event->pos().y());
-    }
+    m_arcball_on = event->button() == Qt::LeftButton;
+    m_zoom_on = event->button() == Qt::RightButton;
+    m_mouse_last = m_mouse_cur = glm::vec2(event->pos().x(), event->pos().y());
 }
 
 void ArcballNavigation::mouseReleaseEvent(QMouseEvent * event)
 {
-    qDebug("release");
-
     if (event->button() == Qt::LeftButton) {
         m_arcball_on = false;
+    }
+    if (event->button() == Qt::RightButton) {
+        m_zoom_on = false;
     }
 }
 
 const glm::mat4 ArcballNavigation::viewMatrixInverted()
 {
-     return glm::inverse(viewMatrix()); // TODO
+     return m_viewMatrixInverted;
 }
+
+
+
--- libs/core/arcballnavigation.h
+++ libs/core/arcballnavigation.h
@@ -13,21 +13,26 @@ public:
     virtual void mouseReleaseEvent(QMouseEvent * event);
 
     virtual const glm::mat4 viewMatrix();
+
+
+
     virtual const glm::mat4 viewMatrixInverted();
     virtual void reset();
 	
 private: 
-    glm::vec3 get_arcball_vector(glm::vec2 v);
+    void updateArcball(); 
+    glm::vec3 getArcballVector(glm::vec2 v);
+    void updateZoom();
 
     glm::vec3 m_center;
     glm::vec3 m_direction;
 
-    glm::vec2 m_last;
-    glm::vec2 m_cur;
+    glm::vec2 m_mouse_last;
+    glm::vec2 m_mouse_cur;
     bool m_arcball_on;
+    bool m_zoom_on;
 
     glm::mat4 m_viewMatrix;
-
-    // source: http://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Arcball
+    glm::mat4 m_viewMatrixInverted;
 };
 
