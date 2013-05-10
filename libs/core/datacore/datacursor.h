ee864ecc49c5c9722b5e86226b7313852a011276
Pavel <pavel.belskiy@student.hpi.uni-potsdam.de>
2013-05-10 12:48:03 +0200

    Derp with command pattern. Somehow we should manage all these update notifications

--- libs/core/datacore/datacursor.h
+++ libs/core/datacore/datacursor.h
@@ -3,5 +3,31 @@
 #pragma once
 
 // TODO
+#include "abstractdata.h"
+
+class AbstractCommand
+{
+public:
+    AbstractCommand();
+    ~AbstractCommand();
+
+    virtual void applyOn(t_DataBlockP dataObj) = 0;
+    virtual AbstractCommand* combineWith(AbstractCommand * cmd);
+};
+
+class CommandQueue
+{
+public:
+    void addCommand(AbstractCommand* );
+    void flush();
+
+};
+
+// class CommandCreate
+// class CommandAdd
+// class CommandSet
+// class CommandView
+// class CommandRemove
+// class CommandDestroy
 
 #endif // DATACORE_DATACURSOR_H
