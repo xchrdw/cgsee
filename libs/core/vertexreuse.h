918d7ce01fa99aa29a6b561a3fb7f5468255d137
kateyy <karsten.tausche@student.hpi.uni-potsdam.de>
2013-05-16 00:04:44 +0200

    #39 Created initial framework for VertexReuse.

---
+++ libs/core/vertexreuse.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "common.h"
+
+class VertexReuse
+{
+public:
+    static void reuseVertices(t_vec3s& m_vertices, t_vec3s& m_normals, t_vec2s& m_texcs, t_uints& m_indices);
+};
+
