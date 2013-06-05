a5ad8e8888d98679668c7bfbb94c6b139f40f63b
Pavel <pavel.belskiy@student.hpi.uni-potsdam.de>
2013-06-05 14:11:28 +0200

    replace config with one from master branch

--- _configure.bat
+++ _configure.bat
@@ -1,5 +1,5 @@
-set GLEW_HOME=C:\libs\glew-1.9.0
-set QT_DIR=C:\Qt\5.0.2
-set GLM_HOME=C:\libs\glm-0.9.3.4
-
-set PATH=%GLEW_HOME%\bin;%QT_DIR%\qtbase\bin;%PATH%;
+set GLEW_HOME=<REPLACE>
+set QT_DIR=<REPLACE>
+set GLM_HOME=<REPLACE>
+set QT_QPA_PLATFORM_PLUGIN_PATH=%QT_DIR%/plugins/platforms
+set PATH=%GLEW_HOME%/bin;%QT_DIR%/bin;%PATH%;
