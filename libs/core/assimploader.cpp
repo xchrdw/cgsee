89d82594b4ce879a8aea8f4aa8fed7e1e0e73368
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2013-06-05 15:52:05 +0200

    ref #9, #10 bypass visual studius inability to support great c++11 features

--- libs/core/assimploader.cpp
+++ libs/core/assimploader.cpp
@@ -17,47 +17,47 @@ AssimpLoader::~AssimpLoader()
 
 QStringList AssimpLoader::loadableTypes() const
 {
-    QStringList types = {
-        "Collada (*.dae *.xml)",
-        "Blender (*.blend)",
-        "Biovision BVH (*.bvh)",
-        "3D Studio Max 3DS (*.3ds)",
-        "3D Studio Max ASE (*.ase)",
-        "Wavefront Object (*.obj)",
-        "Stanford Polygon Library (*.ply)",
-        "AutoCAD DXF (*.dxf)",
-        "IFC-STEP, Industry Foundation Classes (*.ifc)",
-        "Neutral File Format (*.nff)",
-        "Sense8 WorldToolkit (*.wtk)",
-        "Valve Model (*.smd *.vta)",
-        "Quake I (*.mdl)",
-        "Quake II (*.md2)",
-        "Quake III (*.md3)",
-        "Quake 3 BSP (*.pk3)",
-        "RtCW (*.mdc)",
-        "Doom 3 (*.md5mesh *.md5anim *.md5camera)",
-        "DirectX X (*.x)",
-        "Quick3D (*.q3o *.q3s)",
-        "Raw Triangles (.raw)",
-        "AC3D (*.ac)",
-        "Stereolithography (*.stl)",
-        "Autodesk DXF (*.dxf)",
-        "Irrlicht Mesh (*.irrmesh *.xml)",
-        "Irrlicht Scene (*.irr *.xml)",
-        "Object File Format (*.off)",
-        "Terragen Terrain (*.ter)",
-        "3D GameStudio Model (*.mdl)",
-        "3D GameStudio Terrain (*.hmp)",
-        "Ogre (*.mesh.xml, *.skeleton.xml, *.material)",
-        "Milkshape 3D (*.ms3d)",
-        "LightWave Model (*.lwo)",
-        "LightWave Scene (*.lws)",
-        "Modo Model (*.lxo)",
-        "CharacterStudio Motion (*.csm)",
-        "Stanford Ply (*.ply)",
-        "TrueSpace (*.cob *.scn)",
-        "XGL (*.xgl *.zgl)"
-    };
+    QStringList types;
+    types
+        << "Collada (*.dae *.xml)"
+        << "Blender (*.blend)"
+        << "Biovision BVH (*.bvh)"
+        << "3D Studio Max 3DS (*.3ds)"
+        << "3D Studio Max ASE (*.ase)"
+        << "Wavefront Object (*.obj)"
+        << "Stanford Polygon Library (*.ply)"
+        << "AutoCAD DXF (*.dxf)"
+        << "IFC-STEP, Industry Foundation Classes (*.ifc)"
+        << "Neutral File Format (*.nff)"
+        << "Sense8 WorldToolkit (*.wtk)"
+        << "Valve Model (*.smd *.vta)"
+        << "Quake I (*.mdl)"
+        << "Quake II (*.md2)"
+        << "Quake III (*.md3)"
+        << "Quake 3 BSP (*.pk3)"
+        << "RtCW (*.mdc)"
+        << "Doom 3 (*.md5mesh *.md5anim *.md5camera)"
+        << "DirectX X (*.x)"
+        << "Quick3D (*.q3o *.q3s)"
+        << "Raw Triangles (.raw)"
+        << "AC3D (*.ac)"
+        << "Stereolithography (*.stl)"
+        << "Autodesk DXF (*.dxf)"
+        << "Irrlicht Mesh (*.irrmesh *.xml)"
+        << "Irrlicht Scene (*.irr *.xml)"
+        << "Object File Format (*.off)"
+        << "Terragen Terrain (*.ter)"
+        << "3D GameStudio Model (*.mdl)"
+        << "3D GameStudio Terrain (*.hmp)"
+        << "Ogre (*.mesh.xml, *.skeleton.xml, *.material)"
+        << "Milkshape 3D (*.ms3d)"
+        << "LightWave Model (*.lwo)"
+        << "LightWave Scene (*.lws)"
+        << "Modo Model (*.lxo)"
+        << "CharacterStudio Motion (*.csm)"
+        << "Stanford Ply (*.ply)"
+        << "TrueSpace (*.cob *.scn)"
+        << "XGL (*.xgl *.zgl)";
 
     return types;
 }
