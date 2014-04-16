846a2b2570e210424a3e11409665f094c1289fda
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2014-04-16 15:35:52 +0200

    FR added to AUTHORS

--- _configure.bat
+++ _configure.bat
@@ -1,5 +1,6 @@
-set GLEW_HOME=<REPLACE>
-set QT_DIR=<REPLACE>
-set GLM_HOME=<REPLACE>
-set QT_QPA_PLATFORM_PLUGIN_PATH=%QT_DIR%/plugins/platforms
-set PATH=%GLEW_HOME%/bin;%QT_DIR%/bin;%PATH%;
+set GLEW_HOME=C:\Users\Flori\Uni\Semester4\openGL\bin\glew-1.10.0
+set QT_DIR=C:\Qt\qt-5.2.0\qtbase
+set GLM_HOME=C:\Users\Flori\Uni\Semester4\openGL\bin\glm
+set QT_QPA_PLATFORM_PLUGIN_PATH=%QT_DIR%\plugins\platforms
+set ASSIMP_HOME=C:\Users\Flori\Uni\Semester4\openGL\bin\assimp
+set PATH=%GLEW_HOME%\bin;%QT_DIR%\bin;%ASSIMP%\include\x64;%ASSIMP_HOME%\lib\x64;%PATH%;\
