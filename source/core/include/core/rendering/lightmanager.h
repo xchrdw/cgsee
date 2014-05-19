1b182309526ec86e2d73c135f9ac4fa15ea16298
Fabian Dumke <fabian.dumke@student.hpi.uni-potsdam.de>
2014-05-19 16:46:55 +0200

    Fixing a false definition and putting class deklaration in header #142

--- source/core/include/core/rendering/lightmanager.h
+++ source/core/include/core/rendering/lightmanager.h
@@ -13,12 +13,12 @@ class LightManager
 
 public:	
 	directionalLight m_directionalLight;
-	list<pointLight> m_pointLightList;
-	list<spotLight> m_spotLightList;
-	list<goboLight> m_goboLightList;
+	list<pointLight>* m_pointLightList;
+	list<spotLight>* m_spotLightList;
+	list<goboLight>* m_goboLightList;
 
 	void setDirectionalLight(vec3 vector);
-	void addPointLight(pointLight* p_pointLight);
-	void addSpotLight(spotLight* p_spotLight);
-	void addGoboLight(goboLight* p_goboLight);
+	void addPointLight(pointLight p_pointLight);
+	void addSpotLight(spotLight p_spotLight);
+	void addGoboLight(goboLight p_goboLight);
 };
--- source/core/source/rendering/lightmanager.cpp
+++ source/core/source/rendering/lightmanager.cpp
@@ -3,24 +3,12 @@
 
 #include <list>
 
-class LightManager
-{
-	LightManager();
-	virtual ~LightManager();
-
-public:
-	directionalLight m_directionalLight;
-	list<pointLight> m_pointLightList;
-	list<spotLight> m_spotLightList;
-	list<goboLight> m_goboLightList;
-
-	void setDirectionalLight(vec3 vector);
-	void addPointLight(pointLight* p_pointLight);
-	void addSpotLight(spotLight* p_spotLight);
-	void addGoboLight(goboLight* p_goboLight);
 
 LightManager::LightManager()
 {
+	m_pointLightList = new list<pointLight>();
+	m_spotLightList = new list<spotLight>();
+	m_goboLightList = new list<goboLight>();
 }
 
 LightManager::~LightManager()
@@ -32,19 +20,17 @@ void LightManager::setDirectionalLight(vec3 vector)
 	m_directionalLight.m_direction = vector;
 }
 
-void addPointLight(pointLight* p_pointLight)
+void addPointLight(pointLight p_pointLight)
 {
-	m_pointLightList.push_front(&p_pointLight);
+	m_pointLightList->push_front(p_pointLight);
 }
 
-void addSpotLight(spotLight* p_spotLight)
+void addSpotLight(spotLight p_spotLight)
 {
-	m_spotLightList.push_front(&p_spotLight);
+	m_spotLightList->push_front(p_spotLight);
 }
 
-void addGoboLight(goboLight* p_goboLight)
+void addGoboLight(goboLight p_goboLight)
 {
-	m_goboLightList.push_front(&p_goboLight);
-}
-
+	m_goboLightList->push_front(p_goboLight);
 }
