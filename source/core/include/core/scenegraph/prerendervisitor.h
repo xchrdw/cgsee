66b1624f5f43a75903b85d382d3572d5b90817a0
Fabian Dumke <fabian.dumke@student.hpi.uni-potsdam.de>
2014-07-04 09:50:46 +0200

    Adds a new visitor for pretraversing the scenegraph and collecting the light data and setting the camera views. #166

---
+++ source/core/include/core/scenegraph/prerendervisitor.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <core/core_api.h>
+#include <core/scenegraph/scenevisitorinterface.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera;
+class AbstractLight;
+class LightingSystem;
+
+class CORE_API PreRenderVisitor : public SceneVisitorInterface
+{
+public:
+	PreRenderVisitor(LightingSystem & manager, glm::mat4 transform);
+	~PreRenderVisitor();
+
+	bool operator() (Node & node) override;
+	bool operator() (Camera & camera);
+	bool operator() (AbstractLight & light);
+	
+private:
+	LightingSystem & m_manager;
+	glm::mat4 m_transform;
+};
---
+++ source/core/source/scenegraph/prerendervisitor.cpp
@@ -0,0 +1,46 @@
+#include <core/scenegraph/prerendervisitor.h>
+
+#include <core/scenegraph/node.h>
+#include <core/camera.h>
+#include <core/rendering/lightrepresentation/abstractlight.h>
+#include <core/rendering/lightingsystem.h>
+
+
+PreRenderVisitor::PreRenderVisitor(LightingSystem & manager, glm::mat4 t)
+: m_manager(manager)
+, m_transform(t)
+{
+}
+
+PreRenderVisitor::~PreRenderVisitor()
+{
+}
+
+bool PreRenderVisitor::operator() (Node & node)
+{
+	if (node.hidden())
+		return false;
+	return true;
+}
+
+bool PreRenderVisitor::operator() (Camera & camera)
+{
+	if (camera.hidden())
+		return false;
+	camera.setView(m_transform);
+
+	if (Node::RF_Relative == camera.referenceFrame())
+		m_transform *= camera.transform();
+	return true;
+}
+
+bool PreRenderVisitor::operator() (AbstractLight & light)
+{
+	if (light.hidden())
+		return false;
+	light.saveLightData(m_manager, m_transform);
+
+	if (Node::RF_Relative == light.referenceFrame())
+		m_transform *= light.transform();
+	return true;
+}
